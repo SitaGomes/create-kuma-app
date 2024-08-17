970207f7281901737603ec2bff32c62014c4c728
SitaGomes <ansitagomes@gmail.com>
2024-08-17 05:43:33 -0300

    init turborepo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import { Button } from '@kuma/ui/button';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
