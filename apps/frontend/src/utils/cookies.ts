e94485c3988e7bcab428c871d20ac3839046ad8c
SitaGomes <ansitagomes@gmail.com>
2024-09-09 17:53:25 -0300

    fix: remove secret keys

--- apps/frontend/src/utils/cookies.ts
+++ apps/frontend/src/utils/cookies.ts
@@ -1,11 +1,12 @@
 import Cookies from 'js-cookie';
 import CryptoJS from 'crypto-js';
 
-const SECRET_KEY =
-  process.env.SECRET_KEY || 'f47ac10b-58cc-4372-a567-0e02b2c3d479';
+const SECRET_KEY = process.env.SECRET_KEY;
 
 export const getCookie = (key: string) => {
   const hashedValue = Cookies.get(key);
+  if (!SECRET_KEY) return null;
+
   return hashedValue
     ? CryptoJS.AES.decrypt(hashedValue, SECRET_KEY).toString(CryptoJS.enc.Utf8)
     : null;
@@ -16,6 +17,8 @@ export const setCookie = (
   value: string,
   options?: Cookies.CookieAttributes,
 ) => {
+  if (!SECRET_KEY) return;
+
   const hashedValue = CryptoJS.AES.encrypt(value, SECRET_KEY).toString();
   Cookies.set(key, hashedValue, options);
 };
